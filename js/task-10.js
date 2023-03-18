becd7a29101059d5d7344d59a68c56f07fa35be3
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-03-18 18:28:36 +0200

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxEl = document.querySelector("#boxes");
+const inputEl = document.querySelector("[type = 'number']");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+createBtn.addEventListener("click", getAmount);
+destroyBtn.addEventListener("click", destroyBoxes);
+inputEl.addEventListener("input", () => (inputNumber = inputEl.value));
+
+let inputNumber = 0;
+
+function getAmount() {
+  createBoxes(inputNumber);
+}
+
+function createBoxes(amount) {
+  const boxesColumn = [];
+  const firstSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    let newBox = document.createElement("div");
+    newBox.style.width = `${firstSize + i * 10}px`;
+    newBox.style.height = `${firstSize + i * 10}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxesColumn.push(newBox);
+  }
+
+  boxEl.prepend(...boxesColumn);
+}
+
+function destroyBoxes() {
+  boxEl.innerHTML = "";
+}
