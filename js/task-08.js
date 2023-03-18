f1875303d67f42f83b0a389df6865761b6d26210
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-03-18 17:12:10 +0200

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormChange);
+
+function onFormChange(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("Усі поля мають бути заповнені");
+    return;
+  } else {
+    const formData = { email: email.value, password: password.value };
+    console.log(formData);
+  }
+  event.currentTarget.reset();
+}
