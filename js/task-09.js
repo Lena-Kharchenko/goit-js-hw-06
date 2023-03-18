285d7bb08121bc69506cce9b5cdd70c39ac117b1
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-03-18 17:29:42 +0200

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const backGroundEl = document.querySelector(".color");
+
+function changeBackGroundColor() {
+  const backGroundColor = getRandomHexColor();
+
+  bodyColor.style.backgroundColor = `${backGroundColor}`;
+  backGroundEl.textContent = `${backGroundColor}`;
+}
+
+changeColorBtn.addEventListener("click", changeBackGroundColor);
